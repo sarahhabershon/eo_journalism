d7b9e2b21df16019b6ee1533af716df6fdb85618
Sarah <habershon@gmail.com>
2026-02-17 17:17:10 +0100

    main script

---
+++ chart_style.R
@@ -0,0 +1,88 @@
+rseo_style <- function() {
+  font <- "sans"
+  
+  ggplot2::theme(
+    
+    #Text format:
+    #This sets the font, size, type and colour of text for the chart's title
+    plot.title = ggplot2::element_text(family=font,
+                                       size=20,
+                                       face="bold",
+                                       color="#222222"),
+    #This sets the font, size, type and colour of text for the chart's subtitle, as well as setting a margin between the title and the subtitle
+    plot.subtitle = ggplot2::element_text(family=font,
+                                          size=18,
+                                          margin=ggplot2::margin(9,0,9,0)),
+    plot.caption = ggplot2::element_blank(),
+    #This leaves the caption text element empty, because it is set elsewhere in the finalise plot function
+    
+    #Legend format
+    #This sets the position and alignment of the legend, removes a title and backround for it and sets the requirements for any text within the legend. The legend may often need some more manual tweaking when it comes to its exact position based on the plot coordinates.
+    legend.position = "top",
+    legend.text.align = 0,
+    legend.background = ggplot2::element_blank(),
+    legend.title = ggplot2::element_blank(),
+    legend.key = ggplot2::element_blank(),
+    legend.text = ggplot2::element_text(family=font,
+                                        size=10,
+                                        color="#222222"),
+    
+    #Axis format
+    #This sets the text font, size and colour for the axis test, as well as setting the margins and removes lines and ticks. In some cases, axis lines and axis ticks are things we would want to have in the chart - the cookbook shows examples of how to do so.
+    axis.title = ggplot2::element_blank(),
+    axis.text = ggplot2::element_text(family=font,
+                                      size=10,
+                                      color="#282222"),
+    axis.text.x = ggplot2::element_text(margin=ggplot2::margin(5, b = 10)),
+    axis.ticks = ggplot2::element_blank(),
+    axis.line = ggplot2::element_blank(),
+    
+    #Grid lines
+    #This removes all minor gridlines and adds major y gridlines. In many cases you will want to change this to remove y gridlines and add x gridlines. The cookbook shows you examples for doing so
+    panel.grid.minor = ggplot2::element_blank(),
+    panel.grid.major.y = ggplot2::element_line(color="#cbcbcb"),
+    panel.grid.major.x = ggplot2::element_blank(),
+    
+    #Blank background
+    #This sets the panel background as blank, removing the standard grey ggplot background colour from the plot
+    panel.background = ggplot2::element_blank(),
+    
+    #Strip background (#This sets the panel background for facet-wrapped plots to white, removing the standard grey ggplot background colour and sets the title size of the facet-wrap title to font size 22)
+    strip.background = ggplot2::element_rect(fill="white"),
+    strip.text = ggplot2::element_text(size  = 22,  hjust = 0)
+  )
+}
+
+
+five_point_named <- c(
+  "Illustrative"   = "#363432", 
+  "Evidentiary"    = "#196774",  
+  "Investigative"  = "#90A19D", 
+  "Hybrid"         = "#F0941F",
+  "Experimental"   = "#EF6024"   
+)
+
+five_point <- c(
+  "#363432", 
+  "#196774",  
+  "#90A19D", 
+  "#F0941F",
+  "#EF6024"   
+)
+
+alt_cols_five <- c(
+  "#4F7265",   
+  "#D1A34A",  
+  "#B86A45",
+  "#A9B3AC"
+)
+
+seven_point <- c(
+  "#363432",  # Dark gray
+  "#196774",  # Teal-blue
+  "#6C597A",  # Muted eggplant / dusky purple
+  "#F0941F",  # Orange
+  "#EF6024",  # Reddish-orange
+  "#8C2626"   # Muted rose / warm pink
+)
+
